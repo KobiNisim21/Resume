bde6f53af7c5a11fbeae1690a0104ce00b6760c5
Kobin <kobinisim21@gmail.com>
2022-09-10 23:30:02 +0300

    added contact

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,8 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import {MatInputModule} from '@angular/material/input';
 
-const materialModules = [MatIconModule, MatButtonModule];
+const materialModules = [MatIconModule, MatButtonModule, MatInputModule];
 @NgModule({
   declarations: [],
   imports: [CommonModule, ...materialModules],
